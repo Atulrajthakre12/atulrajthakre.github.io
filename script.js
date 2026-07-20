39e7601b1e5cd39fe4da6f11b3fb11c24f67b563
Atul Raj <at3729888@gmail.com>
2026-07-20 17:14:00 +0530

    Add event listener for DOMContentLoaded

---
+++ script.js
@@ -0,0 +1,5 @@
+document.addEventListener("DOMContentLoaded", function () {
+    console.log("Official website loaded successfully!");
+
+    alert("स्वागत है! अतुल राज ठाकरे की आधिकारिक वेबसाइट पर।");
+});
